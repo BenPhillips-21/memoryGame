74af32295302d0fa1379cad616f6cd97f66b80e8
HolidayGuy21 <14449@coderacademy.edu.au>
2024-02-03 12:00:00 +1100

    added more monkey gifs, did more styling

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
 
 function Cards() {
-  let allMonkeys = ['hoppyMonkey.gif', 'berryMonkey.gif', 'miniMonkey.gif', 'shooterMonkey.gif', 'swagMonkey.gif','bathMonkey.gif','businessMonkey.gif','cheekyMonkey.gif','cycleMonkey.gif','dogMonkey.gif','prettyMonkey.gif','skaterMonkey.gif','slideMonkey.gif','spinMonkey.gif','zenMonkey.gif','devMonkey.gif','ironingMonkey.gif','blackMonkey.gif','puppetMonkey.gif','rangaMonkey.gif'];
+  let allMonkeys = ['animatedMonkey.gif', 'beanieMonkey.gif', 'boatMonkey.gif', 'crazyMonkey.gif', 'danceMonkey.gif', 'shirtMonkey.gif', 'hoppyMonkey.gif', 'berryMonkey.gif', 'miniMonkey.gif', 'shooterMonkey.gif', 'swagMonkey.gif','bathMonkey.gif','businessMonkey.gif','cheekyMonkey.gif','cycleMonkey.gif','dogMonkey.gif','prettyMonkey.gif','skaterMonkey.gif','slideMonkey.gif','spinMonkey.gif','zenMonkey.gif','devMonkey.gif','ironingMonkey.gif','blackMonkey.gif','puppetMonkey.gif','rangaMonkey.gif'];
   const [monkeys, setMonkeys] = useState(() => randomTen(allMonkeys));
   const [clickedMonkeys, setClickedMonkeys] = useState([]);
   const [score, setScore] = useState(0)
-  const [highScore, setHighScore] = useState()
+  const [highScore, setHighScore] = useState(0)
 
   useEffect(() => {
     const shuffledMonkeys = shuffleArray(monkeys);
@@ -15,6 +15,11 @@ function Cards() {
   function handleClick(gif) {
     if (clickedMonkeys.includes(gif)) {
       console.log("Monkey clicked already");
+      if (score > highScore || highScore === 0) { setHighScore(score) }
+      setScore(0)
+      let newMonkeys = randomTen(allMonkeys)
+      setMonkeys(newMonkeys)
+      setClickedMonkeys([])
       return;
     }
     let newScore = score + 1
@@ -30,7 +35,7 @@ function Cards() {
       const randomIndex = Math.floor(Math.random() * (i + 1));
       [newArray[i], newArray[randomIndex]] = [newArray[randomIndex], newArray[i]];
     }
-    const selectedElements = newArray.slice(0, 10);
+    const selectedElements = newArray.slice(0, 14);
     return selectedElements;
   }
 
@@ -45,15 +50,21 @@ function Cards() {
 
   return (
     <>
-      <h1>Score: {score}</h1>
-      <h1>High Score: {highScore}</h1>
-      {monkeys.map((gif, index) => (
-        <div key={index} onClick={() => handleClick(`/gifs/${gif}`)} style={{ cursor: 'pointer' }}>
-          <img src={`/gifs/${gif}`} alt={`Gif ${index + 1}`} />
-        </div>
-      ))}
+    <h1 style={{ marginTop: '-15px' }}>Monkey Memory Game</h1>
+    <h4>Don't click the same monkey twice!</h4>
+    <div style={{ display: 'flex', justifyContent: 'center' }}>
+      <h4 style={{ marginTop: '0px', marginLeft: '30px', marginRight: '30px' }}>Score: {score}</h4>
+      <h4 style={{ marginTop: '0px' }}>High Score: {highScore}</h4>
+      </div>
+      <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
+        {monkeys.map((gif, index) => (
+          <div key={index} onClick={() => handleClick(`/gifs/${gif}`)} style={{ cursor: 'pointer', margin: '5px' }}>
+            <img src={`/gifs/${gif}`} alt={`Gif ${index + 1}`} style={{ border: '3px solid black', width: '150px', height: '150px' }} />
+          </div>
+        ))}
+      </div>
     </>
-  );
+  );   
 }
 
 export default Cards;
