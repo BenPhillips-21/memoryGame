176903aa2ceb5580d37147fdceb4bfedcbd4965c
HolidayGuy21 <14449@coderacademy.edu.au>
2024-02-07 12:00:00 +1100

    changed favicon / title

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Cards from './components/cards.jsx'
+import { Helmet } from 'react-helmet';
 
 function App() {
 
   return (
     <>
+            <Helmet>
+        <title>Monkey Memory Game</title>
+        <link rel="icon" type="image/png" href="cool.png" sizes="32x32" />
+      </Helmet>
       <Cards></Cards>
     </>
   )
